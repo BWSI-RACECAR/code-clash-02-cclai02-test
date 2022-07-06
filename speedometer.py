59831e75eced392db7eb40694c9e2bc6988d2506
cclai02-test <108826004+cclai02-test@users.noreply.github.com>
2022-07-06 11:21:16 -0700

    Create speedometer.py

---
+++ speedometer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def two_numbers(ary,target):
+        # type ary: list
+        # type target: int
+        # return type: list or bool
+
+        for x in ary:
+            other_num = target - x
+            if other_num in ary:
+                return [ary.index(x), ary.index(other_num)]
+        return False
+
+def main():
+    array = input()
+    target = input()
+    tc1 = Solution()
+
+    print(tc1.two_numbers(array, target))
+
+if __name__ == "__main__":
+    main()
